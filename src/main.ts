eee422a8c0b5dc4469508aab89ed5c8193948cb9
James Hibberd <james.d.hibberd@gmail.com>
2023-10-13 05:43:54 +0000

    simple web app using express

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,40 @@
 import { Connection } from "postgresql-client";
+import express, { NextFunction, Request, Response } from "express";
 
 const connection = new Connection(
   "postgres://postgres:postgres@localhost/sticky",
 );
-await connection.connect();
-const result = await connection.query("select * from collections where id = 1");
-const rows: any[] = result.rows!;
-console.log(rows);
-await connection.close();
+const port = 8080;
+
+async function get(): Promise<string> {
+  await connection.connect();
+  const result = await connection.query(
+    "select * from collections where id = 1",
+  );
+  const rows: any[] = result.rows!;
+  await connection.close();
+  return rows[0];
+}
+
+const app = express();
+
+app.get("/", async (_req, res) => {
+  let data = await get();
+  res.type("text/plain");
+  res.send("Hey there\n" + data);
+});
+
+app.use((_req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("Not found");
+});
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.message);
+  res.type("text/plain");
+  res.status(500);
+  res.send("Server Error");
+});
+
+app.listen(port, () => console.log("Express started"));
